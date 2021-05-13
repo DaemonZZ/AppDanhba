fbdddfb11b4fe299bf4e2ec3ac7f08de8404d8dd
Katsuo Saitou <sgooophx@gmail.com>
2021-05-13 13:29:00 +0700

    complete

---
+++ app/src/main/java/com/contact/Call_Log.java
@@ -0,0 +1,40 @@
+package com.contact;
+
+public class Call_Log {
+    private String number;
+    private String duration;
+    private String time;
+
+    public Call_Log() {
+    }
+
+    public Call_Log(String number, String duration, String time) {
+        this.number = number;
+        this.duration = duration;
+        this.time = time;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
---
+++ app/src/main/java/com/contact/LogAdapter.java
@@ -0,0 +1,63 @@
+package com.contact;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class LogAdapter extends RecyclerView.Adapter {
+    private List<Call_Log> list;
+    private Context context;
+
+    public LogAdapter(List<Call_Log> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.log_item,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Call_Log log =list.get(position);
+        ((ViewHolder)holder).txtNum.setText(log.getNumber());
+        ((ViewHolder)holder).txtDur.setText(log.getDuration());
+        ((ViewHolder)holder).txtTime.setText(log.getTime());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtNum,txtDur,txtTime;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            txtNum = itemView.findViewById(R.id.txtNumber);
+            txtDur = itemView.findViewById(R.id.txtDuration);
+            txtTime = itemView.findViewById(R.id.txtTime);
+        }
+    }
+}
